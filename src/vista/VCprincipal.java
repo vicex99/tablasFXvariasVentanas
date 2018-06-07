520aace231e916dbada33d927db632ea5658df5d
victor <victor.alvarez@live.u-tad.com>
2018-06-07 21:28:47 +0200

    commit para diferenciar ramas

--- src/vista/VCprincipal.java
+++ src/vista/VCprincipal.java
@@ -15,50 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class VCprincipal implements Initializable {
-	
-	private Main ProgramaPrincipal;
-
-	@FXML
-	private Button btnAceptar;
-	
-	@FXML
-	private Button btnEditar;
-	
-	@FXML
-	private Button btnBorrar;
-
-	@FXML
-	private TableView<Persona> miTabla;
-
-	@FXML
-	private TableColumn<Persona, String> colName;
-	
-	@FXML
-	private TableColumn<Persona, String> colApe;
-	
-	@FXML
-	private TableColumn<Persona, Integer> colTf;
-
-	private final ObservableList<Persona> data = FXCollections.observableArrayList(
-			new Persona("Javier", "Sanchez", 234534639), 
-			new Persona("Víctor", "Álvarez", 256435734)
-		);
-
-	// Relaciona el main con el controlador
-	public void setProgramaPrincipal(Main ProgramaPrincipal) {
-		this.ProgramaPrincipal = ProgramaPrincipal;
-	}
 
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		miTabla.setItems(this.data);
-		colName.setCellValueFactory(new PropertyValueFactory<Persona, String>("Nombre"));
-		colApe.setCellValueFactory(new PropertyValueFactory<Persona, String>("Apellido"));
-		colTf.setCellValueFactory(new PropertyValueFactory<Persona, Integer>("telefono"));
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
-
-	@FXML
-	private void ventanaIniciar() {
-		this.ProgramaPrincipal.mostrarVentanaPrincipal();
-	}
+	
+	
 }
